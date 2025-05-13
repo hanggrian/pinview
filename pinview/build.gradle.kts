152107232bc2d2d66d5336864d5a209dd852ec3c
Hendra Anggrian <hanggrian@proton.me>
2025-05-13 09:23:11 -0500

    Republished to a new Maven point, updated sample app to showcase in Pinterest and Layers

--- pinview/build.gradle.kts
+++ pinview/build.gradle.kts
@@ -9,11 +9,25 @@ plugins {
     alias(libs.plugins.maven.publish)
 }
 
+checkstyle.toolVersion = libs.versions.checkstyle.get()
+
 android {
     namespace = "$releaseGroup.$releaseArtifact"
     testNamespace = "$namespace.test"
     buildFeatures.buildConfig = false
     testOptions.unitTests.isIncludeAndroidResources = true
+
+    tasks.register<Javadoc>("javadocAndroid") {
+        source = sourceSets["main"].java.getSourceFiles()
+        classpath += files(bootClasspath)
+        classpath +=
+            libraryVariants
+                .find { it.name == "release" }!!
+                .javaCompileProvider
+                .get()
+                .classpath
+        setDestinationDir(layout.buildDirectory.dir("docs/${project.name}").get().asFile)
+    }
 }
 
 dependencies {
@@ -22,4 +36,60 @@ dependencies {
     implementation(libs.androidx.appcompat)
 
     testImplementation(libs.bundles.androidx.test)
+}
+
+tasks {
+    val checkstyleAndroid by registering(Checkstyle::class) {
+        group = LifecycleBasePlugin.VERIFICATION_GROUP
+        description = "Generate Android lint report"
+
+        source("src")
+        include("**/*.java")
+        exclude("**/gen/**", "**/R.java")
+        classpath = files()
+    }
+    named("check") {
+        dependsOn(checkstyleAndroid)
+    }
+
+    withType<Test>().configureEach {
+        configure<JacocoTaskExtension> {
+            isIncludeNoLocationClasses = true
+            excludes = listOf("jdk.internal.*")
+        }
+    }
+    register<JacocoReport>("jacocoAndroid") {
+        group = "Reporting"
+        description = "Generate Android test coverage"
+
+        dependsOn("testDebugUnitTest", "connectedDebugAndroidTest")
+        mustRunAfter("test")
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+        sourceDirectories.setFrom(layout.projectDirectory.dir("src/main/java"))
+        classDirectories.setFrom(
+            files(
+                fileTree(layout.buildDirectory.dir("intermediates/javac/")) {
+                    exclude(
+                        "**/R.class",
+                        "**/R\$*.class",
+                        "**/BuildConfig.*",
+                        "**/Manifest*.*",
+                        "**/*Test*.*",
+                        "**/*Args.*",
+                        "**/*Directions.*",
+                    )
+                },
+            ),
+        )
+        executionData.setFrom(
+            files(
+                fileTree(layout.buildDirectory) {
+                    include("**/*.exec", "**/*.ec")
+                }
+            ),
+        )
+    }
 }
